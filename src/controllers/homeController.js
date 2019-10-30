9955c838e3d998e6c2e38e9f07752d71493ecadf
Viet Binh <nvbinh.halutech@gmail.com>
2019-10-30 15:14:22 +0700

    5.5

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,11 @@
-let gethome = (req, res) => {
+import { notify } from "./../services/index"
+let gethome = async(req, res) => {
+    let notifications = await notify.getNotifiCations(req.user._id);
     return res.render("main/home/home", {
         errors: req.flash("errors"), //định nghĩa errors là gì rồi truyền ra views
         success: req.flash("success"), //định nghĩa success là gì rồi truyền ra views
         user: req.user,
+        notifications: notifications,
     });
 };
 module.exports = {
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,8 @@
 import authservices from './authservices';
 import userservices from "./userservice";
 import FindUsersContact from "./contactService";
+import getNotifiCations from "./notifiCationService";
 export const auth = authservices;
 export const user = userservices;
 export const contact = FindUsersContact;
+export const notify = getNotifiCations;
