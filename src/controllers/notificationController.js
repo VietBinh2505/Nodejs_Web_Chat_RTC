29f6b6df6901f5b0e84994dd04c0a809685f9924
Viet Binh <nvbinh.halutech@gmail.com>
2019-11-01 15:57:18 +0700

    5.8

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -10,6 +10,16 @@ let readMore = async(req, res) => { //xem nhiều hơn
         res.status(500).send(error);
     }
 };
+let markAllRead = async(req, res) => { //xem nhiều hơn 
+    try {
+        let mark = notify.markAllRead(req.user._id, req.body.targetUser);
+        return res.status(200).send(mark);
+    } catch (error) {
+        console.log(error);;
+        res.status(500).send(error);
+    }
+};
 module.exports = {
     readMore: readMore,
+    markAllRead: markAllRead,
 };
