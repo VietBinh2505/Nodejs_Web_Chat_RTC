4536db0e46a5fdf8584a7122331c9eac2a494276
VietBinh2505 <nvbinh.halutech@gmail.com>
2019-11-15 23:06:21 +0700

    6.6

---
+++ src/public/js/textAndEmojiChat.js
@@ -0,0 +1,24 @@
+function textAndEmojiChat(divId) {
+	$(".emojionearea").unbind("keyup").on("keyup", function(element){
+		if(element.which === 13){
+			let targetId = $(`#write-chat-${divId}`).data("chat"); //lấy ra id của nhóm trò chuyện hoặc cá nhân
+			let messageVal = $(`#write-chat-${divId}`).val(); //lấy ra nội dung tin nhắn user nhập
+			if(!targetId.length || !messageVal.length){
+				return false;
+			}
+			let dataTextEmojiForSend = {
+				uid: targetId,//lấy ra id của nhóm trò chuyện hoặc cá nhân
+				messageVal: messageVal, //lấy ra nội dung tin nhắn user nhập
+			};
+			if($(`#write-chat-${divId}`).hasClass("chat-in-group")){
+				dataTextEmojiForSend.isChatGroup = true;
+			}
+			$.post("/message/add-new-text-emoji",dataTextEmojiForSend, function(data){
+				
+			}).fail(function(response){ //nếu có lỗi
+				console.log("loi tai textAndEmojiChat/js/public");
+				console.log(response);
+			});
+		}
+	});
+};
